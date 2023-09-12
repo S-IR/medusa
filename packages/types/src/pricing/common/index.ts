41e1e8d6a1741372069e617d4d6e13011ab701fa
Philip Korsholm <philip.korsholm@hotmail.com>
2023-09-12 09:50:49 +0200

    suggestions from pr review

--- packages/types/src/pricing/common/index.ts
+++ packages/types/src/pricing/common/index.ts
@@ -1,8 +1,5 @@
 export * from "./currency"
 export * from "./money-amount"
-<<<<<<< HEAD
 export * from "./price-set"
 export * from './price-list'
-=======
-export * from "./price-list"
->>>>>>> bb182b0c6 (suggestions from pr review)
+
